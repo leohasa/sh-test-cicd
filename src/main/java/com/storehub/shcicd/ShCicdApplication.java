ff953a3877894ba1c5d316dc6f6ef6a561e988c2
L2AsHdz <leonidas0197@gmail.com>
2024-10-17 18:18:57 -0600

    initial commit

---
+++ src/main/java/com/storehub/shcicd/ShCicdApplication.java
@@ -0,0 +1,13 @@
+package com.storehub.shcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/storehub/shcicd/ShCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storehub.shcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
